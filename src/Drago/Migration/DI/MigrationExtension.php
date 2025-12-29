40bb0ae11ad2c27b2f3f6b16dff779077796c33e
accgit <zdenek.papucik@gmail.com>
2025-12-29 18:33:08 +0100

    dev

--- src/Drago/Migration/DI/MigrationExtension.php
+++ src/Drago/Migration/DI/MigrationExtension.php
@@ -11,27 +11,51 @@ namespace Drago\Migration\DI;
 
 use Drago\Migration\MigrationCommand;
 use Drago\Migration\MigrationRunner;
-use Drago\Migration\Repository;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\ServiceDefinition;
+use Symfony\Component\Console\Command\Command;
 
 
 final class MigrationExtension extends CompilerExtension
 {
+	private bool $consoleMode;
+
+
+	public function __construct(bool $consoleMode = false)
+	{
+		$this->consoleMode = $consoleMode;
+	}
+
+
 	public function loadConfiguration(): void
 	{
+		if (!$this->consoleMode) {
+			return;
+		}
+
 		$builder = $this->getContainerBuilder();
-
-		// Repository
-		$builder->addDefinition($this->prefix('repository'))
-			->setFactory(Repository::class);
-
-		// MigrationRunner
 		$builder->addDefinition($this->prefix('runner'))
-			->setFactory(MigrationRunner::class, [$this->prefix('@repository')]);
+			->setFactory(MigrationRunner::class);
 
-		// MigrationCommand
 		$builder->addDefinition($this->prefix('command'))
-			->setFactory(MigrationCommand::class, [$this->prefix('@runner')])
+			->setFactory(MigrationCommand::class, ['@' . $this->prefix('runner')])
 			->addTag('console.command');
+
+		$this->compiler->addExportedType(MigrationCommand::class);
+	}
+
+
+	public function beforeCompile(): void
+	{
+		if (!$this->consoleMode) {
+			return;
+		}
+
+		$builder = $this->getContainerBuilder();
+		$commands = $builder->findByType(Command::class);
+		foreach ($commands as $serviceName => $serviceDef) {
+			assert($serviceDef instanceof ServiceDefinition);
+			$serviceDef->addTag('console.command');
+		}
 	}
 }
